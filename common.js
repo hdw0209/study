4459e9b291faa771c14b249d1a9d3c7367ddc8e6
헨더기 <henduki@gmail.com>
2020-12-11 15:36:22 +0900

    1211

--- common.js
+++ common.js
@@ -1,53 +1,21 @@
-function includeHTML() {
-    var z, i, elmnt, file, xhttp;
-    /* Loop through a collection of all HTML elements: */
-    z = document.getElementsByTagName('*');
-    for (i = 0; i < z.length; i++) {
-        elmnt = z[i];
-        /*search for elements with a certain atrribute:*/
-        file = elmnt.getAttribute('include-html');
-        if (file) {
-            /* Make an HTTP request using the attribute value as the file name: */
-            xhttp = new XMLHttpRequest();
-            xhttp.onreadystatechange = function () {
-                if (this.readyState == 4) {
-                    if (this.status == 200) {
-                        elmnt.innerHTML = this.responseText;
-                    }
-                    if (this.status == 404) {
-                        elmnt.innerHTML = 'Page not found.';
-                    }
-                    /* Remove the attribute, and call this function once more: */
-                    elmnt.removeAttribute('include-html');
-                    includeHTML();
-                }
-            };
-            xhttp.open('GET', file, true);
-            xhttp.send();
-            /* Exit the function: */
-            return;
-        }
-    }
-}
-includeHTML();
-
-function openNav() {
-    document.getElementById('sideNav').style.width = '300px';
-    document.getElementById('main').style.marginLeft = '300px';
-}
-
-function closeNav() {
-    document.getElementById('sideNav').style.width = '0';
-    document.getElementById('main').style.marginLeft = '0';
-}
+$('[include-html]').each((idx, el) => {
+    el = $(el);
+    el.load(el.attr('include-html'));
+});
 
 function setActive() {
     linkObj = document.getElementById('sideNav').getElementsByTagName('a');
-    for (i = 0; i < linkObj.length; i++) {
-        if (document.location.href.indexOf(linkObj[i].href) >= 0) {
-            linkObj[i].classList.add('active');
+
+    let url = document.location.pathname;
+    url = url.substr(0, url.length - 1).replace(/\%20/g, ' ');
+
+    document.querySelectorAll('#sideNav a').forEach((a) => {
+        console.log(url, a.getAttribute('href'), url === a.getAttribute('href'));
+        if (url === a.getAttribute('href')) {
+            a.classList.add('active');
+            a.parentElement.parentElement.parentElement.parentElement.children[0].classList.add('active');
         }
-    }
+    });
 }
 
 window.onload = setActive;
